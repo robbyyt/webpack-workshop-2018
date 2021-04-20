bafe41769c704d0ddf4d6c04dd342a9f50dbaa31
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2021-04-20 15:46:42 +0300

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
-module.exports = ({ mode }) => {
-  console.log(mode);
-  return { 
-    mode,
-    output: {
-      filename: "bundle.js"
+module.exports = ({ mode, presets } = { mode: "production", presets: []}) => {
+  return merge({
+      mode,
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-  }
-}
+    modeConfig(mode),
+  );
+} 
