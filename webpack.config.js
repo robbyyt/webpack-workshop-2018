bef254f003be5debb79aa10b0f779bd8dddf0207
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2021-04-20 15:16:08 +0300

    play

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  mode: "none"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  console.log(env);
+
+  return {
+    mode: env.mode,
+    output: {
+    filename: "bundle.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.ProgressPlugin()
+    ]
+  }
 };
